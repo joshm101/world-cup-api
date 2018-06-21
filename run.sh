3bcc27a341d97b3fa38019b7b4d1387982289395
Joshua Mora <jshm101@gmail.com>
2018-06-21 16:57:06 -0700

    proper port exposure

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker kill world-cup-api-container
 docker rm world-cup-api-container
 docker build -t world-cup-api --build-arg WCA_MONGODB_CONNECTION_URI=${WCA_MONGODB_CONNECTION_URI} --build-arg WCA_MONGODB_CONNECTION_USERNAME=${WCA_MONGODB_CONNECTION_USERNAME} --build-arg WCA_MONGODB_CONNECTION_PASSWORD=${WCA_MONGODB_CONNECTION_PASSWORD} .
-docker run -d --name world-cup-api-container world-cup-api
+docker run -p 3000:3000 -d --name world-cup-api-container world-cup-api
 docker logs -f world-cup-api-container
