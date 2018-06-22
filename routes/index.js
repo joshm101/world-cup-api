ab1ca1cf017c800a074515f6513bcd8b74848282
Joshua Mora <jshm101@gmail.com>
2018-06-21 23:11:14 -0700

    set CORS headers

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const api = require('./api')
 
+router.use((req, res, next) => {
+  // Set CORS headers
+  res.append('Access-Control-Allow-Origin', ['*'])
+  res.append('Access-Control-Allowed-Methods', 'GET')
+  res.append('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 router.use('/api', api)
 
 module.exports = router;
